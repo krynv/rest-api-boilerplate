deb62304ae5c4bbf60cbae21d27a691a6185c087
VK <24637852+krynv@users.noreply.github.com>
2024-01-31 18:49:24 +0000

    Added an 'updateUser' route

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,6 +1,11 @@
 import express from 'express';
 
-import { deleteUserById, getUsers } from '../db/users';
+import {
+  deleteUserById,
+  getUserById,
+  getUsers,
+  updateUserById
+} from '../db/users';
 
 export const getAllUsers = async (req: express.Request, res: express.Response) => {
   try {
@@ -19,6 +24,30 @@ export const deleteUser = async (req: express.Request, res: express.Response) =>
     const deletedUser = await deleteUserById(id);
 
     return res.status(200).json(deletedUser).end();
+  } catch (error) {
+    console.error(error);
+    return res.sendStatus(400);
+  }
+};
+
+export const updateUser = async (req: express.Request, res: express.Response) => {
+  try {
+    const { id } = req.params;
+    const { username } = req.body;
+
+    if (!username || !id) {
+      return res.sendStatus(400);
+    }
+
+    const user = await getUserById(id);
+
+    if (!user) {
+      return res.sendStatus(404);
+    }
+
+    const updatedUser = await updateUserById(id, { username });
+
+    return res.status(200).json(updatedUser).end();
   } catch (error) {
     console.error(error);
     return res.sendStatus(400);
--- src/router/users.ts
+++ src/router/users.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 
-import { deleteUser, getAllUsers } from '../controllers/users';
+import { deleteUser, getAllUsers, updateUser } from '../controllers/users';
 import { isAuthenticated, isOwner } from '../middleware';
 
 export default (router: express.Router): express.Router => {
   router.get('/users', isAuthenticated, getAllUsers);
   router.delete('/users/:id', isAuthenticated, isOwner, deleteUser);
+  router.patch('/users/:id', isAuthenticated, isOwner, updateUser);
 
   return router;
 };
