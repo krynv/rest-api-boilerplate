b609942112c3023a8d2fc07e7817373924c2a8e3
VK <24637852+krynv@users.noreply.github.com>
2024-01-31 11:00:29 +0000

    Updated the console text

--- src/index.ts
+++ src/index.ts
@@ -20,5 +20,5 @@ app.use(cors({
 const server = http.createServer(app);
 
 server.listen(PORT, () => {
-  console.log(`Server is running on port http://localhost:${PORT}/`);
+  console.log(`Server is running on: http://localhost:${PORT}/`);
 });
