9d817cc5346a1a9b1fd463178d1fbfc611bb3972
VK <24637852+krynv@users.noreply.github.com>
2024-01-31 10:18:54 +0000

    Created a basic HTTP server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log('Hello World');
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(compression());
+
+app.use(cors({
+  credentials: true,
+}));
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}/`);
+});
